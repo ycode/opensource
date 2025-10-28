1469e0c79e2decac3fd08265bc10474100b7f49e
Ignas Lunėnas <34475426+lunenas@users.noreply.github.com>
2025-10-28 15:09:15 +0200

    Layout WIP

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-muted-foreground hover:text-secondary-foreground hover:bg-secondary/80",
         ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 text-muted-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
