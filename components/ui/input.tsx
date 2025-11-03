6e2ada3ec078c0b5f4ea41e1f03bf8bd7b562a90
Ignas Lunėnas <34475426+lunenas@users.noreply.github.com>
2025-11-03 15:55:08 +0200

    Input placeholder

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -8,7 +8,7 @@ function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
       type={type}
       data-slot="input"
       className={cn(
-        'file:text-foreground placeholder:text-placeholder selection:bg-primary selection:text-primary-foreground bg-input border-transparent h-8 w-full min-w-0 rounded-lg border px-2 py-1 text-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
+        'file:text-foreground placeholder:text-current/25 selection:bg-primary selection:text-primary-foreground bg-input border-transparent h-8 w-full min-w-0 rounded-lg border px-2 py-1 text-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50',
         'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[0px]',
         '',
         'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
