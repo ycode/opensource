f1a091ded2b00102a31e737d2619fe466001f4bc
Ignas Lunėnas <34475426+lunenas@users.noreply.github.com>
2026-01-13 15:40:36 +0200

    Hero 002

--- lib/templates/layouts.ts
+++ lib/templates/layouts.ts
@@ -366,4 +366,251 @@ export const layoutTemplates: Record<string, LayoutTemplate> = {
       'customName': 'Section'
     },
   },
+
+  'hero-002': {
+    category: 'Hero',
+    previewImage: '/layouts/hero-002.webp',
+    template: {
+      'name': 'section',
+      'design': {
+        'layout': {
+          'display': 'Flex',
+          'isActive': true,
+          'alignItems': 'center',
+          'flexDirection': 'column'
+        },
+        'spacing': {
+          'isActive': true,
+          'paddingTop': '140',
+          'paddingBottom': '140'
+        }
+      },
+      'classes': 'flex flex-col items-center pt-[140px] pb-[140px]',
+      'children': [
+        {
+          'name': 'div',
+          'design': {
+            'layout': {
+              'display': 'Flex',
+              'isActive': true,
+              'alignItems': 'center',
+              'flexDirection': 'column'
+            },
+            'sizing': {
+              'width': '100%',
+              'isActive': true,
+              'maxWidth': '1280px'
+            },
+            'spacing': {
+              'isActive': true,
+              'paddingLeft': '32px',
+              'paddingRight': '32px'
+            }
+          },
+          'classes': 'flex flex-col max-w-[1280px] w-[100%] pl-[32px] pr-[32px] items-center',
+          'children': [
+            {
+              'name': 'div',
+              'design': {
+                'layout': {
+                  'gap': '16px',
+                  'display': 'Flex',
+                  'isActive': true,
+                  'alignItems': 'center',
+                  'flexDirection': 'column'
+                },
+                'sizing': {
+                  'width': '[100%]',
+                  'isActive': true,
+                  'maxWidth': '680'
+                }
+              },
+              'classes': 'flex flex-col gap-[16px] items-center w-[100%] max-w-[680px]',
+              'children': [
+                {
+                  'name': 'heading',
+                  'design': {
+                    'typography': {
+                      'fontSize': '36',
+                      'isActive': true,
+                      'textAlign': 'center',
+                      'fontWeight': '700',
+                      'lineHeight': '1.1',
+                      'letterSpacing': '-0.02'
+                    }
+                  },
+                  'classes': 'text-[48px] font-[700] leading-[1.1] tracking-[-0.02em] text-center max-md:text-[36px]',
+                  'children': [],
+                  'settings': {
+                    'tag': 'h1'
+                  },
+                  'variables': {
+                    'text': {
+                      'data': {
+                        'content': 'Experience content management reimagined'
+                      },
+                      'type': 'dynamic_text'
+                    }
+                  },
+                  'customName': 'Heading',
+                  'restrictions': {
+                    'editText': true
+                  }
+                },
+                {
+                  'name': 'p',
+                  'design': {
+                    'typography': {
+                      'color': '#000000/60',
+                      'isActive': true,
+                      'textAlign': 'center'
+                    }
+                  },
+                  'classes': 'text-[20px] text-[#000000]/60 text-center',
+                  'children': [],
+                  'variables': {
+                    'text': {
+                      'data': {
+                        'content': 'Let copywriters easily work with content via content management system or visually on canvas without breaking design.'
+                      },
+                      'type': 'dynamic_text'
+                    }
+                  },
+                  'customName': 'Paragraph',
+                  'restrictions': {
+                    'editText': true
+                  }
+                },
+                {
+                  'name': 'div',
+                  'classes': 'flex gap-ąč gap-[8px]',
+                  'children': [
+                    {
+                      'name': 'button',
+                      'classes': 'flex flex-row items-center justify-center text-[#FFFFFF] pr-[20px] pl-[20px] pt-[10px] pb-[10px] text-[16px] rounded-[12px] bg-[#171717]',
+                      'children': [
+                        {
+                          'name': 'span',
+                          'classes': 'text-[16px]',
+                          'restrictions': {
+                            'editText': true
+                          },
+                          'children': [],
+                          'design': {
+                            'typography': {
+                              'isActive': true,
+                              'fontSize': '16px'
+                            }
+                          },
+                          'variables': {
+                            'text': {
+                              'type': 'dynamic_text',
+                              'data': {
+                                'content': 'Get started'
+                              }
+                            }
+                          }
+                        }
+                      ],
+                      'attributes': {
+                        'type': 'button'
+                      },
+                      'design': {
+                        'typography': {
+                          'isActive': true,
+                          'color': '#ffffff',
+                          'fontSize': '16px'
+                        },
+                        'spacing': {
+                          'isActive': true,
+                          'paddingLeft': '1.5rem',
+                          'paddingRight': '1.5rem',
+                          'paddingTop': '0.5rem',
+                          'paddingBottom': '0.5rem'
+                        },
+                        'borders': {
+                          'isActive': true,
+                          'borderRadius': '0.5rem'
+                        },
+                        'backgrounds': {
+                          'isActive': true,
+                          'backgroundColor': '#2563eb'
+                        }
+                      },
+                      'customName': 'Button'
+                    },
+                    {
+                      'name': 'button',
+                      'classes': 'flex flex-row items-center justify-center pr-[20px] pl-[20px] pt-[10px] pb-[10px] text-[16px] rounded-[12px] text-[#171717] bg-[#e5e5e5]',
+                      'children': [
+                        {
+                          'name': 'span',
+                          'classes': 'text-[16px]',
+                          'restrictions': {
+                            'editText': true
+                          },
+                          'children': [],
+                          'design': {
+                            'typography': {
+                              'isActive': true,
+                              'fontSize': '16px'
+                            }
+                          },
+                          'variables': {
+                            'text': {
+                              'type': 'dynamic_text',
+                              'data': {
+                                'content': 'Learn more'
+                              }
+                            }
+                          }
+                        }
+                      ],
+                      'attributes': {
+                        'type': 'button'
+                      },
+                      'design': {
+                        'borders': {
+                          'isActive': true,
+                          'borderRadius': '0.5rem'
+                        },
+                        'spacing': {
+                          'isActive': true,
+                          'paddingTop': '0.5rem',
+                          'paddingLeft': '1.5rem',
+                          'paddingRight': '1.5rem',
+                          'paddingBottom': '0.5rem'
+                        },
+                        'typography': {
+                          'color': '#171717',
+                          'fontSize': '16px',
+                          'isActive': true
+                        },
+                        'backgrounds': {
+                          'isActive': true,
+                          'backgroundColor': '#e5e5e5'
+                        }
+                      },
+                      'customName': 'Button'
+                    }
+                  ],
+                  'design': {
+                    'layout': {
+                      'isActive': true,
+                      'display': 'Flex',
+                      'gap': '8'
+                    }
+                  },
+                  'customName': 'Columns'
+                }
+              ],
+              'customName': 'Rows'
+            }
+          ],
+          'customName': 'Container'
+        }
+      ],
+      'customName': 'Section'
+    },
+  },
 };
